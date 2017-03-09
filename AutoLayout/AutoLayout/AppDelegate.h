764bda123c13fb87d0dea2196a21eee9c95123aa
Glauber Lima <glauberlima@gmail.com>
2017-03-09 10:18:40 -0300

    AutoLayout

---
+++ AutoLayout/AutoLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayout
+//
+//  Created by Glauber Lima on 14/02/17.
+//  Copyright © 2017 CI&T. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoLayout/AutoLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayout
+//
+//  Created by Glauber Lima on 14/02/17.
+//  Copyright © 2017 CI&T. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
